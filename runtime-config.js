3315ade3315fe8d826785e5429ea8f68caafad4c
ArTeGaS-2 <probablytorvik@gmail.com>
2026-02-15 19:07:58 +0200

    Update runtime-config 2026-02-15 19:07:57

--- runtime-config.js
+++ runtime-config.js
@@ -1,7 +1,7 @@
 ﻿(function () {
   window.QUIZ_RUNTIME_CONFIG = {
-    api: 'https://397b-176-37-206-160.ngrok-free.app',
-    ws: 'wss://397b-176-37-206-160.ngrok-free.app',
-    updatedAt: '2026-02-15T19:02:33.9898398+02:00',
+    api: 'https://77b7-176-37-206-160.ngrok-free.app',
+    ws: 'wss://77b7-176-37-206-160.ngrok-free.app',
+    updatedAt: '2026-02-15T19:07:57.6314035+02:00',
   };
 })();
